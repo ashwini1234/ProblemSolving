ed68387933ebd029a964e0d87996f1e18af7a6e0
ashwini1234 <ashwinipoojary.ace@gmail.com>
2022-01-15 20:06:55 +0530

    Update MaxSumContiguousArray.java

--- MaxSumContiguousArray.java
+++ MaxSumContiguousArray.java
@@ -1,3 +1,14 @@
+//Find the contiguous subarray within an array, A of length N which has the largest sum
+
+//Problem Constraints:
+//1 <= N <= 1e6 , -1000 <= A[i] <= 1000
+
+//Input 1: A = [1, 2, 3, 4, -10] 
+//Output 1: 10   {1,2,3,4}
+
+//Input 2: A = [-2, 1, -3, 4, -1, 2, 1, -5, 4] 
+//Output 2:6  {4, -1, 2, 1}
+
 public class Solution {
     
     public int maxSubArray(final int[] A) {
@@ -15,3 +26,6 @@ public class Solution {
         return ans;
     }
 }
+
+//  Time Complexity: O(N)
+// Space Complexity: O(1)
